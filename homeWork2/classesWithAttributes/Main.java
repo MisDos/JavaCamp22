13059463a70da17f7218fe74cb26cb69adcd4437
MisDos <113919460+MisDos@users.noreply.github.com>
2022-10-10 13:12:42 +0300

    Add files via upload

---
+++ homeWork2/classesWithAttributes/Main.java
@@ -0,0 +1,22 @@
+package classesWithAttributes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Product product = new Product();
+		product.name = "PC";
+		product.setId(61);
+		product.description = "Jamirakuaaa PC!";
+		product.price = 61500;
+		product.stockAmount = 5;
+		
+		
+		ProdcutManager productManager= new ProdcutManager();
+		productManager.Add(product);
+		
+	
+		
+
+	}
+
+}
---
+++ homeWork2/classesWithAttributes/Product.java
@@ -0,0 +1,64 @@
+package classesWithAttributes;
+
+public class Product {
+	int id;
+	String name;
+	String description;
+	double price;
+	int stockAmount;
+	private String colour;
+	private String code;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public int getStockAmount() {
+		return stockAmount;
+	}
+	public void setStockAmount(int stockAmount) {
+		this.stockAmount = stockAmount;
+	}
+	public String getColour() {
+		return colour;
+	}
+	public void setColour(String colour) {
+		this.colour = colour;
+	}
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	
+	//getter
+	
+		
+	
+	
+		
+	}
+	
+	
+	
+
