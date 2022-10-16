0d4419e9226207cc14d587b244a02d429563804f
MisDos <113919460+MisDos@users.noreply.github.com>
2022-10-16 13:39:57 +0300

    JavaCampDay3

---
+++ oopWithNLayeredApp/dataAccess/JdbcProductDao.java
@@ -0,0 +1,12 @@
+package oopWithNLayeredApp.dataAccess;
+
+import oopWithNLayeredApp.entities.Product;
+
+public class JdbcProductDao implements ProductDao {
+	public void add(Product product) {
+		System.out.println("Added to database with JDBC ");
+		
+	}
+	
+
+}
